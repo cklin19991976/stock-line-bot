7e6a47273ce7515fc85b15b88c5ecca42d831bf0
cklin19991976 <cklin_1999@yahoo.com>
2026-03-24 22:50:56 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -20,9 +20,15 @@ USER_ID = os.getenv("USER_ID")
 
 # ===== CONFIG =====
 SYMBOLS = {
-    "AAPL": 200,
-    "TSLA": 250,
-    "NVDA": 270
+    "AAPL": 300,
+    "SPY": 614,
+    "QQQ": 540,
+    "TSM": 300,
+    "ASML": 1250,
+    "UCO": 39.66,
+    "GOOG": 276,
+    "MSFT": 347,	
+    "NVDA": 160
 }
 CHECK_INTERVAL = 60  # seconds
 COOLDOWN = 1800  # seconds per symbol
@@ -55,8 +61,8 @@ def check_stock(symbol, threshold):
         now = time.time()
         last_time = last_alert_time.get(symbol, 0)
 
-        if price > threshold and (now - last_time > COOLDOWN):
-            msg = f"🚨 {symbol} crossed {threshold}\nCurrent: {round(price,2)}"
+        if price < threshold and (now - last_time > COOLDOWN):
+            msg = f"🚨 {symbol} below {threshold}\nCurrent: {round(price,2)}"
             print(msg)
             send_line(msg)
             last_alert_time[symbol] = now
